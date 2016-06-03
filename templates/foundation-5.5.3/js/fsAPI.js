c6f3eb95b22e00198c34f78c2dbf7878ac23fd98
Uno95 <unouo84@ymail.com>
2016-06-03 10:12:57 +0700

    latitude and longitude created

--- templates/foundation-5.5.3/js/fsAPI.js
+++ templates/foundation-5.5.3/js/fsAPI.js
@@ -1,14 +1,27 @@
+var len, itms, lt, lg;
 var clienId = "14CSP1SYZHDG1WKGU2R02S0AUE44RWRSQEOSHPDVHGNBNDR0";
 var clientSecret = "B5G0BNCLMIS1E2DJELSC2SA54HMNEBKWZUDRZSQFLAKQDLE3";
-
-var items, lt, lg;
+locationObj = [];
+locProps = {};
 
 var loc = $("#resName").text();
 
-var gecodeAPI = "https://api.foursquare.com/v2/venues/explore?client_id="+ clienId +"&client_secret="+ clientSecret +"&mode=url&section=food&near="+loc;
+var gecodeAPI = "https://api.foursquare.com/v2/venues/explore?client_id="+ clienId +"&client_secret="+ clientSecret +"&mode=url&section=food&v=20130815&near="+loc;
 console.log(gecodeAPI);
 
 $.getJSON(gecodeAPI, function(json) {
-	items = json.response.groups[0].items[0];
-	console.log(items);
+	itemObj = json.response.groups[0].items;
+	len = itemObj.length;
+	for (i = 0; i < len; i++) {
+		itms = json.response.groups[0].items[i];
+		lt = itms.venue.location.lat;
+		lg = itms.venue.location.lng;
+		
+		locProps.lat = lt;
+		locProps.lng = lg;
+		locationObj.push(locProps);
+
+		console.log("Lat: "+lt+" "+"Long: "+lg);
+	}
+	console.log(locationObj);
 });
