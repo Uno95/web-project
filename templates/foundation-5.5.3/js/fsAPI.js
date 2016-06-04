2a409cbf16b3da42d1b903715db9e4211aceec3b
ridhogusti0 <ridhogusti0@gmail.com>
2016-06-04 13:53:10 +0700

    Update Marker Map

--- templates/foundation-5.5.3/js/fsAPI.js
+++ templates/foundation-5.5.3/js/fsAPI.js
@@ -6,7 +6,8 @@ locProps = {};
 
 var loc = $("#resName").text();
 
-var gecodeAPI = "https://api.foursquare.com/v2/venues/explore?client_id="+ clienId +"&client_secret="+ clientSecret +"&mode=url&section=food&v=20130815&near="+loc;
+var test=[];
+var gecodeAPI = "https://api.foursquare.com/v2/venues/explore?client_id="+ clienId +"&client_secret="+ clientSecret +"&mode=url&section=coffee&v=20130815&near="+loc;
 console.log(gecodeAPI);
 
 $.getJSON(gecodeAPI, function(json) {
@@ -16,13 +17,13 @@ $.getJSON(gecodeAPI, function(json) {
 		itms = json.response.groups[0].items[i];
 		lt = itms.venue.location.lat;
 		lg = itms.venue.location.lng;
-		
 		locProps.lat = lt;
 		locProps.lng = lg;
 		locationObj.push(locProps);
 
-		console.log("Lat: "+lt+" "+"Long: "+lg);
+		console.log(lt+" "+": "+lg);
 	}
 	initMap(lt, lg);
 	console.log(locationObj);
 });
+$('#resNamee').text(itms.venue.location.lat);
