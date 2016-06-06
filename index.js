44186921aa28ad770e38df9cbe3d248c2d941896
Uno95 <unouo84@ymail.com>
2016-06-06 15:24:00 +0700

    oauthIO

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var qr = require('qr-image'),
 	express = require('express'),
+	OAuth = require('oauthio'),
 	middleware = require('./middleware'),
 	app = express();
 
 middleware(app);
+OAuth.initialize('KEWL365dK7FzbXttgCD5eYh-wCk', 'jzWKfuIVfPVU3WStw6YlPiEbIpI');
 app.use('/', express.static(__dirname + '/templates'));
 app.use('/', express.static(__dirname + '/imgres'));
 app.set('port', process.env.PORT || 3000);
