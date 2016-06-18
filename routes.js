8102cda843b52dd70155d3efe705a1c7f66d16cc
Uno95 <unouo84@ymail.com>
2016-06-18 08:55:17 +0700

    routes

---
+++ routes.js
@@ -0,0 +1,36 @@
+var express = require('express'),
+	r = express.Router(),
+	h = require('./handler'),
+	router;
+
+router = function(app, passport){
+	r.get('/', h.home);
+	r.get('/form', h.form);
+	r.get('/formsign', h.formsign);
+	r.get('/login', h.login);
+	r.get('/berhasilregister', h.berhasilregister);
+	r.get('/hasilpencarian/:name', h.hasilpencarian);
+	r.get('/place/:name/:id', h.placeDetail);
+	r.get('/beranda/:username', h.userpage);
+
+	r.post('/newuser',passport.authenticate('local-signup', {
+	    successRedirect : '/userpage', // redirect to the secure profile section
+	    failureRedirect : '/formsign', // redirect back to the signup page if there is an error
+	    failureFlash : true // allow flash messages
+	}));
+
+	r.post('/loginp',passport.authenticate('local-login', {
+	    successRedirect : '/userpage', // redirect to the secure profile section
+	    failureRedirect : '/login', // redirect back to the signup page if there is an error
+	    failureFlash : true // allow flash messages
+	}));
+
+	app.use(r);
+};
+module.exports = router;
+
+function isLoggedin(req,res,next){
+	if(req.isAuthenticated())
+		return next();
+	res.redirect('/');
+}
